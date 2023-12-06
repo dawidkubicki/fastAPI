5c05cfd67df7ee4e006fea1bc91b3d5ad6428864
Dawid Kubicki <dawid@pluscode.io>
2023-12-06 09:48:59 +0100

    guard eendpoint

--- 3_MySimpleTodoApp/routers/todos.py
+++ 3_MySimpleTodoApp/routers/todos.py
@@ -28,8 +28,17 @@ class TodoRequest(BaseModel):
     complete: bool
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
+async def read_all(user: user_dependency,db: db_dependency):
+    return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
+
+@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(user: user_dependency,db: db_dependency, todo_id: int):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not get todo. User not authorized.")
+    try:
+        return db.query(Todos).filter(Todos.owner_id == user.get('id')).filter(Todos.id==todo_id).first()
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo of requested id not found.")
 
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
@@ -38,4 +47,31 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_request: To
     todo_model = Todos(**todo_request.model_dump(), owner_id=user.get('id'))
     
     db.add(todo_model)
+    db.commit()
+
+@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not get access")
+    todo_model = db.query(Todos).filter(Todos.owner_id == user.get('id')).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not get access")
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id==user.get('id')).first()
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id==user.get('id')).delete()
     db.commit()
